e7dafa28eb338c8f7c42f2b44a06f246331bba01
William Pitcock <nenolod@dereferenced.org>
2014-12-14 11:41:49 -0600

    add setup infrastructure.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup, Extension
+
+setup (
+    name='ircreactor',
+    version='0',
+    packages=['ircreactor'],
+    description='library for manipulating and reacting to IRC messages as mutable state',
+    author='William Pitcock',
+    author_email='nenolod@dereferenced.org',
+    url='http://kaniini.dereferenced.org/projects/ircreactor.html'
+)
+
